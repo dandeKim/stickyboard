f3b6306c87598a3faaec3235c574fc0e749eafa4
dande <ofkande@gmail.com>
2020-09-06 12:12:44 +0900

    Add dummy data

--- src/components/sticker/RechartGithubCalendar.js
+++ src/components/sticker/RechartGithubCalendar.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import { GitHubCalendar } from '@stickyboard/github-calendar';
 
-function RechartsGithubCalendar(props) {
-    return <GitHubCalendar>test</GitHubCalendar>;
+const calendarData = [
+    { date: '2020-01-01', count: 125 },
+    { date: '2020-01-22', count: 21 },
+    { date: '2020-01-30', count: 30 },
+    { date: '2020-02-11', count: 40 },
+    { date: '2020-02-12', count: 2 },
+    { date: '2020-02-13', count: 33 },
+    { date: '2020-02-15', count: 24 },
+    { date: '2020-02-30', count: 23 },
+    { date: '2020-03-20', count: 14 },
+    { date: '2020-05-24', count: 45 },
+    { date: '2020-11-24', count: 43 },
+    { date: '2020-12-24', count: 12 },
+    { date: '2020-12-25', count: 3 },
+    { date: '2020-12-28', count: 95 },
+];
+
+function RechartsGithubCalendar() {
+    return (
+        <GitHubCalendar
+            startDate="2020-01-01"
+            endDate="2020-12-31"
+            data={calendarData}
+        />
+    );
 }
 
 export default RechartsGithubCalendar;
